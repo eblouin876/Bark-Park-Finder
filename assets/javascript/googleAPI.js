96eb6f60e56587addfd38e2639c932ab7f811ba5
ivancho277 <rouskoi@live.com>
2019-03-17 14:53:10 -0700

    wip

--- assets/javascript/googleAPI.js
+++ assets/javascript/googleAPI.js
@@ -1,5 +1,25 @@
 // api key AIzaSyC7vLUfavKg4fYmtRJOKm_QfbyQxD-8jJM
-
-class googleAPI(){
+// api key AIzaSyC7vLUfavKg4fYmtRJOKm_QfbyQxD-8jJM
+// <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC7vLUfavKg4fYmtRJOKm_QfbyQxD-8jJM&callback=initMap"></script>
+var map;
+var currentLocation;
+var seattle = {lat: 47.6062 , lng:-122.3321 };
+currentLocation = seattle;
+function initMap() {
     
+    map = new google.maps.Map(document.getElementById('map'), {zoom: 12, center: seattle});
+    var marker = new google.maps.Marker({position: seattle, map: map});
+}
+function addMarker(coords){
+    var marker = new google.maps.Marker({
+        position: coords,
+        map:map,
+        icon: "" //add marker img link
+    })
+
+    var request = {
+        location: pyrmont,
+        radius: '500',
+        query: 'off leash dog park'
+      };
 }
