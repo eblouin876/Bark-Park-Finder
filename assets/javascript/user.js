71bb38ecbcb3fe291c25ee9a7f78c7bd62d26167
Sammy Tang <samtang226@gmail.com>
2019-03-19 14:01:12 -0700

    added comments for readability

--- assets/javascript/user.js
+++ assets/javascript/user.js
@@ -1,5 +1,8 @@
-class userCard{
-    constructor(username, contact, picture, bio, dog, location){
+//class for the cards from users
+class userCard {
+
+    //constructor takes in a username, email, picture, bio, dog, and location
+    constructor(username, contact, picture, bio, dog, location) {
         this.username = username;
         this.contact = contact;
         this.picture = picture;
@@ -7,28 +10,54 @@ class userCard{
         this.dog = dog;
         this.location = location;
     }
-    
-    buildTile(parentID, modalID){
+
+    //buildTile takes in a parent ID that is a div where the cards are going to be placed
+    //it also takes in a div to place modals with the information of the user
+    buildTile(parentID, modalID) {
         let button = $("<button>")
-        .attr("id", this.username);
+            .attr("id", this.username);
         let image = $("<img>")
-        .addClass("user-image")
-        .attr("src", this.picture);
+            .addClass("user-image")
+            .attr("src", this.picture);
         button.append(image);
         $(parentID).append(button);
 
         let modalContent = $("<div>")
-        .addClass("modal-content");
+            .addClass("modal-content");
 
         let span = $("<span>")
-        .addClass("close")
-        .html('&times;');
+            .addClass("close")
+            .html('&times;');
 
         let para = $("<p>")
-        .text(this.bio);
+            .text(this.bio);
 
         modalContent.append(span)
         modalContent.append(para)
         $(modalID).append(modalContent);
+    }
+}
+
+//modalUserCard takes a user and adds info to their modal based on user information
+function modalUserCard(user) {
+    var modal = document.getElementById('myModal');
+    var btn = document.getElementById(user.username);
+    var span = document.getElementsByClassName("close")[0];
+
+    btn.onclick = function () {
+        modal.style.display = "block";
+        console.log("work")
+    }
+
+    span.onclick = function () {
+        modal.style.display = "none";
+        console.log("work")
+    }
+
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+            console.log("work")
+        }
     }
 }
--- assets/javascript/userdata.js
+++ assets/javascript/userdata.js
@@ -1,17 +1,29 @@
+//userdata.js is used for creating new users
+
+//initilize database
 var db = firebase.firestore();
+
+//get current user
 var user = firebase.auth().currentUser;
+
+//variables to get inputs from user in textboxes
 var username = document.getElementById("username");
 var dog = document.getElementById("dog");
 var picture = document.getElementById("profilepic");
 var bio = document.getElementById("bio");
 var submit = document.getElementById("submit");
+
+//use this to get the picture name later
 var profilePic
 
-firebase.auth().onAuthStateChanged(function(user){
+//on changes to user state
+firebase.auth().onAuthStateChanged(function (user) {
 
-    if(user){
+    //if user is logged in
+    if (user) {
 
-        picture.addEventListener("change", function(e) {
+        //when user selects an image as a profile picture, add it to the firebase storage
+        picture.addEventListener("change", function (e) {
             var file = e.target.files[0];
             profilePic = file;
             // if(firebase.storage().ref("userProfiles/" + user.uid)){
@@ -19,12 +31,13 @@ firebase.auth().onAuthStateChanged(function(user){
             //     firebase.storage().ref("userProfiles/" + user.uid).delete();
             // }
 
-            var storageRef = firebase.storage().ref("userProfiles/" + user.uid +"/" + file.name);
+            var storageRef = firebase.storage().ref("userProfiles/" + user.uid + "/" + file.name);
 
             storageRef.put(file);
         });
 
-        submit.addEventListener("click", e =>{
+        //when user hits submit, store all values into firestore
+        submit.addEventListener("click", e => {
             var useremail = user.email;
             console.log(useremail);
             var uid = user.uid;
@@ -36,20 +49,18 @@ firebase.auth().onAuthStateChanged(function(user){
             var bioValue = bio.value;
             console.log(bioValue);
 
-            console.log(profilePic)
+            //this is to get the reference of the picture insde of firebase storage
             var storage = firebase.storage();
-            var pathReference = storage.ref("userProfiles/" + uid +"/" + profilePic.name);
+            var pathReference = storage.ref("userProfiles/" + uid + "/" + profilePic.name);
 
-            console.log(pathReference);
-
-            var newUser = new userCard(userValue, useremail, pathReference, bioValue, dogValue, "here");
-
+            //adding values to database
             db.collection("users").add({
                 email: useremail,
                 uid: uid,
                 username: userValue,
                 dog: dogValue,
                 bio: bioValue,
+                pic: pathReference
             })
 
 
