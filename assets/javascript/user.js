9450436471f860fb5f36bf90d32d7a58aa1c2d10
Sammy Tang <samtang226@gmail.com>
2019-03-19 14:38:03 -0700

    mostly working, needs styling

--- assets/javascript/user.js
+++ assets/javascript/user.js
@@ -25,15 +25,30 @@ class userCard {
         let modalContent = $("<div>")
             .addClass("modal-content");
 
+        let modalHeader = $("<div>")
+            .addClass("modal-header");
+        let modalBody = $("<div>")
+            .addClass("modal-body");
+        let modalFooter = $("<div>")
+            .addClass("modal-footer");
         let span = $("<span>")
             .addClass("close")
             .html('&times;');
 
+        let displayUser = $("<h2>")
+            .text(this.username);
         let para = $("<p>")
-            .text(this.bio);
+            .text(this.bio + this.dog + this.contact);
+        let profilePic = $("<img>")
+            .attr("src", this.picture);
 
-        modalContent.append(span)
-        modalContent.append(para)
+        modalHeader.append(span);
+        modalHeader.append(displayUser);
+        modalBody.append(para);
+        modalBody.append(profilePic);
+        modalContent.append(modalHeader);
+        modalContent.append(modalBody);
+        modalContent.append(modalFooter);
         $(modalID).append(modalContent);
     }
 }
