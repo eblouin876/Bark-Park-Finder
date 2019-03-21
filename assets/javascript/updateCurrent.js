487753f7d5f163953940d1ac8d74d3b756297606
Emile Blouin <emile_blouin@alumni.brown.edu>
2019-03-20 22:54:35 -0700

    Added 45 minute timeout in updateCurrent for parkgoers

--- assets/javascript/updateCurrent.js
+++ assets/javascript/updateCurrent.js
@@ -4,7 +4,17 @@ db.collection('parkUsers').onSnapshot(function (querySnapshot) {
     // Clear the buttonDiv and myModal ids
     querySnapshot.forEach((doc) => {
         let val = doc.data()
-        // returns val.bio, val.dog, val.email, val.location, val.pic, val.uid, val.username
+        // returns val.bio, val.dog, val.email, val.location, val.checkInTime val.pic, val.uid, val.username
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        let currentTime = hours + (minutes / 60)
+        if (currentTime - val.checkInTime > .75 || currentTime - val.checkInTime < 0) {
+            db.collection('parkUsers').doc(doc.id).update({
+                location: "",
+                checkInTime: ""
+            })
+            val.location = ""
+        }
         if (val.location === $('#check-in').attr('data-location')) {
             // make new card and display it.
         }
@@ -18,6 +28,16 @@ function updateCurrentPark() {
         querySnapshot.forEach((doc) => {
             let val = doc.data()
             // returns val.bio, val.dog, val.email, val.location, val.pic, val.uid, val.username
+            let hours = date.getHours();
+            let minutes = date.getMinutes();
+            let currentTime = hours + (minutes / 60)
+            if (currentTime - val.checkInTime > .75 || currentTime - val.checkInTime < 0) {
+                db.collection('parkUsers').doc(doc.id).update({
+                    location: "",
+                    checkInTime: ""
+                })
+                val.location = ""
+            }
             if (val.location === $('#check-in').attr('data-location')) {
                 // make new card and display it.
             }
@@ -25,11 +45,3 @@ function updateCurrentPark() {
 
     })
 }
-
-
-
-// Could pass in where("parkId", "===", "parkId") Later to get specific ones
-//  admin.firestore().collection('parkUsers').get().then((querySnapshot) => {
-//     querySnapshot.forEach((doc) => {
-//         console.log("Document data:", doc.data())
-//     })
